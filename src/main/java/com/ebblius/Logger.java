417774cd1a677b9c376b7d7d1e77aa47936e8b73
unsubble <unsubble@gmail.com>
2024-09-01 14:50:40 +0300

    Logger class written.

--- src/main/java/com/ebblius/Logger.java
+++ src/main/java/com/ebblius/Logger.java
@@ -1,14 +1,37 @@
 package com.ebblius;
 
+import org.apache.logging.log4j.LogManager;
+
 public class Logger {
 
-    void warning(){}
+    private static Logger instance;
+    private final org.apache.logging.log4j.Logger logger;
 
-    void info(){}
+    private Logger() {
+        logger = LogManager.getLogger();
+    }
 
-    void error(){}
+    public void warn(Object message) {
+        logger.warn(message);
+    }
 
-    void fatal(){}
+    public void info(Object message) {
+        logger.info(message);
+    }
 
+    public void error(Object message) {
+        logger.error(message);
+    }
 
+    public void fatal(Object message) {
+        logger.fatal(message);
+    }
+
+    public static Logger getInstance() {
+        synchronized (Logger.class) {
+            if (instance == null)
+                instance = new Logger();
+        }
+        return instance;
+    }
 }
