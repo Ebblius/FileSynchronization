91092e86677e574dc2952422e332e61c9de9c2d7
KeremYagiz124 <keremyagizkarakas1@gmail.com>
2024-09-02 17:18:57 +0300

    File comparator and file copier system added

--- src/main/java/com/ebblius/FileComparator.java
+++ src/main/java/com/ebblius/FileComparator.java
@@ -1,15 +1,52 @@
 package com.ebblius;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class FileComparator {
 
-    boolean compareFiles(File file1, File file2){return false;}
+    boolean compareFiles(File file1, File file2) throws FileNotFoundException, IOException{
+        
+        if(file1.length() != file2.length() || file1.lastModified() != file2.lastModified())
+            return false;
+        
+        BufferedReader bReader1 = new BufferedReader(new FileReader(file1));
+        BufferedReader bReader2 = new BufferedReader(new FileReader(file2));
+        
+        String line1 = "", line2 = "";
+        while((line1 = bReader1.readLine()) != null && ((line2 = bReader2.readLine()) != null)){
+            if(line1 == null ? line2 != null : !line1.equals(line2))
+                return false;
+        }
+        return line1 == null && line2 == null;
+    }
 
-    List<String> getDifferences(){return null;}
-
-
-
-
-}
+    List<String> getDifferences(File file1, File file2) throws FileNotFoundException, IOException{
+        List<String> lines1 = new LinkedList();
+        List<String> lines2 = new LinkedList();
+        
+        BufferedReader bReader1 = new BufferedReader(new FileReader(file1));
+        BufferedReader bReader2 = new BufferedReader(new FileReader(file2));
+        
+        String line;
+        while((line = bReader1.readLine()) != null)
+        lines1.add(line);
+        while((line = bReader2.readLine()) != null)
+        lines1.add(line);
+        
+        List<String> differences = new LinkedList<>(lines1);
+        differences.addAll(lines2);
+        List<String> commonLines = new LinkedList<>(lines2);
+        commonLines.retainAll(lines2);
+        differences.removeAll(commonLines);
+        
+        return differences;
+    }
+ }
--- src/main/java/com/ebblius/FileCopier.java
+++ src/main/java/com/ebblius/FileCopier.java
@@ -1,16 +1,45 @@
 package com.ebblius;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileCopier {
 
-    void copyFile(Path source, Path destination){
-
+    void copyFile(File source, File destination) throws FileNotFoundException, IOException{
+        
+        BufferedReader bReader = new BufferedReader( new FileReader(source));
+        BufferedWriter bWriter = new BufferedWriter( new FileWriter(destination));
+        
+        if(!source.exists())
+            throw new IOException("Kaynak dosya bulunamadı." + source.getAbsolutePath());
+        if(!destination.exists())
+            destination.createNewFile();
+        
+        String line;
+        while((line = bReader.readLine()) != null){
+            bWriter.write(line);
+        }
     }
 
-    void copyFiles(File files, File destination){}
+    void copyFiles(File[] sourceFiles, File destination) throws IOException{
+        
+            if (!destination.isDirectory()) 
+            throw new IllegalArgumentException("Hedef dosya bir differenceszin olmalıdır: " + destination.getAbsolutePath());
+            
+            for(File sourceFile: sourceFiles){
+                if (!sourceFile.exists()) {
+                    System.err.println("Kaynak dosya bulunamadı: " + sourceFile.getAbsolutePath());
+                    continue;
+            }
+                File destFile = new File(destination, sourceFile.getName());
+                copyFile(sourceFile, destFile);
+        }
+    }
 
 
 }
